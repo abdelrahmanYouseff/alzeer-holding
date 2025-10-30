7417d11093b76acd7984cd885b12386d40ad0f4e
AYoussef00 <abdelrahman.yousef@hadaf-hq.com>
2025-10-30 08:36:41 +0300

    edit

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -6,7 +6,7 @@
         <div class="header-content">
           <!-- Logo -->
           <div class="logo-section">
-            <img src="/asset/logos/44.png" alt="Alzeer Group Holding" class="logo-image" />
+            <img src="/Screenshot_1447-03-29_at_10.51.05_AM-removebg-preview.png" alt="Alzeer Group Holding" class="logo-image" />
         </div>
 
           <!-- Navigation - Desktop -->
@@ -195,7 +195,7 @@
                 <svg class="contact-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                 </svg>
-                <span class="contact-text">+96655 512 6435</span>
+                <span class="contact-text" dir="ltr">+96655 512 6435</span>
               </div>
               <div class="footer-contact-item">
                 <svg class="contact-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -817,7 +817,7 @@ const labels = computed(() => ({
   font-size: 2.5rem;
   font-weight: 700;
   margin-bottom: 1.5rem;
-  color: #a97b2c;
+  color: #d9bb59;
   line-height: 1.2;
 }
 
